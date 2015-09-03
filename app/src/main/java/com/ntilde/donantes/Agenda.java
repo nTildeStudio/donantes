6d015a92f4ab796b6740ee1ac6e20f2045794c40
emaleavil <emaleavil@gmail.com>
2015-09-03 21:31:12 +0200

    calendario

--- app/src/main/java/com/ntilde/donantes/Agenda.java
+++ app/src/main/java/com/ntilde/donantes/Agenda.java
@@ -1,16 +1,45 @@
 package com.ntilde.donantes;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.ntilde.percentagelayout.PLinearLayout;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class Agenda extends ActionBarActivity {
+public class Agenda extends AppCompatActivity {
+
+    @InjectView(R.id.iconos_margen_superior) PLinearLayout ic_margen_sup;
+    @InjectView(R.id.configuracion_logotipo)ImageView logotipo;
+    @InjectView(R.id.configuracion_borde_rojo_superior) PLinearLayout borde_rojo_superior;
+    @InjectView(R.id.configuracion_borde_rojo_inferior) LinearLayout borde_rojo_inferior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.activity_open_translate,R.anim.activity_close_scale);
         setContentView(R.layout.activity_agenda);
+
+        ButterKnife.inject(this);
+
+        ic_margen_sup.post(new Runnable() {
+            @Override
+            public void run() {
+                int valor = ic_margen_sup.getPHeight();
+                logotipo.setPadding(valor, valor / 2, valor, valor / 2);
+            }
+        });
+
+        borde_rojo_superior.post(new Runnable() {
+            @Override
+            public void run() {
+                borde_rojo_inferior.getLayoutParams().height = borde_rojo_superior.getPHeight();
+            }
+        });
     }
 
     @Override
