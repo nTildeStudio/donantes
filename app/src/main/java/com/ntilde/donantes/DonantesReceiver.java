8a87df0ad10f8d7968559af6da7b78420d56fdfc
juliome10 <juliogomezreis@gmail.com>
2015-09-03 21:15:37 +0200

    Mensajes funcionando

---
+++ app/src/main/java/com/ntilde/donantes/DonantesReceiver.java
@@ -0,0 +1,58 @@
+package com.ntilde.donantes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by 0011361 on 03/09/2015.
+ */
+public class DonantesReceiver extends ParsePushBroadcastReceiver{
+
+    public static final String PARSE_DATA_KEY = "com.parse.Data";
+
+    @Override
+    protected void onPushReceive(Context context, Intent intent) {
+        super.onPushReceive(context, intent);
+
+        Log.i("XXX", "Notificación recibida");
+        JSONObject data = null;
+        try {
+            data = new JSONObject(intent.getExtras().getString(PARSE_DATA_KEY));
+
+            SharedPreferences prefs = context.getSharedPreferences(Constantes.SP_KEY, Configuracion.MODE_PRIVATE);
+
+            Set<String> alertas=prefs.getStringSet(Constantes.SP_ALERTAS,null);
+
+            if(alertas==null){
+                Log.e("XXX","lo tenemos, pero nulo");
+            }
+            else{
+                Log.e("XXX","lo tenemos y no es nulo");
+                for(String alerta:alertas){
+                    Log.e("XXX","Tiene: "+alerta);
+                }
+            }
+
+            alertas.add(System.currentTimeMillis() + "::" + data.getString("alert"));
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putStringSet(Constantes.SP_ALERTAS, alertas);
+            editor.commit();
+
+        } catch (JSONException e) {
+            Log.i("XXX", "Error al parsear la notificacion");
+        }
+    }
+
+}
